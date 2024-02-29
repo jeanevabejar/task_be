3494f3a57599d57fe0ac86596bf9ddb33129427a
jeanbejar <jeanevabejar@gmail.com>
2024-02-29 10:59:22 +0800

    feat: implement complete api service for frontend

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # GET /categories/:category_id/tasks
+  def index
+    @tasks = @category.tasks
+    render json: @tasks
+  end
+
+  # GET /categories/:category_id/tasks/1
+  def show
+    render json: @task
+  end
+
+  # POST /categories/:category_id/tasks
+  def create
+    @task = @category.tasks.build(task_params)
+
+    if @task.save
+      render json: @task, status: :created, location: [@category, @task]
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /categories/:category_id/tasks/1
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /categories/:category_id/tasks/1
+  def destroy
+    @task.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = current_user.categories.find(params[:category_id])
+    end
+
+    def set_task
+      @task = @category.tasks.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  has_many :categories
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+   
+         def jwt_payload
+          super
+        end      
+end
