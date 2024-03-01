cdb5ee8697e4f41ff3969d2a874a527aad0d3139
jeanbejar <jeanevabejar@gmail.com>
2024-03-01 16:47:59 +0800

    refactor: create boolean default value to task column completed and permit completed to update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_025704) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_082804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_025704) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
