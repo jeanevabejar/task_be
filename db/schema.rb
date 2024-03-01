c53d13e504b76bdb4d5cdc2bb2529d1c1e57e1c8
jeanbejar <jeanevabejar@gmail.com>
2024-03-01 11:10:05 +0800

    refactor: this add boolean condition for completed task.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_162342) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_025704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_162342) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
