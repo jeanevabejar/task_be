d218877488e7aae58656b6e8b19a8bb708a0adf0
jeanbejar <jeanevabejar@gmail.com>
2024-03-06 16:58:04 +0800

    fix: the test integration.

--- test/integration/user_registration_test.rb
+++ test/integration/user_registration_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class UserRegistrationTest < ActionDispatch::IntegrationTest
   test "registers a new user" do
-    post "/user", params: { user: { email: 'test@example.com', password: 'password123', password_confirmation: 'password123' } }
+    post "/users", params: { user: { email: 'test@example.com', password: 'password123', password_confirmation: 'password123' } }
 
-    assert_response :created
+    assert_response :success
     assert_equal 1, User.count
 
   end
