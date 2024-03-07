690c5077fee9808cb4d344c149b67768ef386d3e
jeanbejar <jeanevabejar@gmail.com>
2024-03-07 13:02:15 +0800

    refactor: undo the cors file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://task-pocq8uba3-jeanbejars-projects.vercel.app"
+    origins "*"
 
     resource "*",
       headers: :any,
