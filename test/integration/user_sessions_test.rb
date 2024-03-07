4824395b7485a0ea73c3cad629fbedb372e4a314
jeanbejar <jeanevabejar@gmail.com>
2024-03-07 16:15:42 +0800

    refactor: the test.

--- test/integration/user_sessions_test.rb
+++ test/integration/user_sessions_test.rb
@@ -1,25 +1,21 @@
 require "test_helper"
 
 class UserSessionsTest < ActionDispatch::IntegrationTest
-  test "registers a new user and login" do
+  test "registers a new user, login,  and category" do
     post "/users", params: { user: { email: 'test@example.com', password: 'password123', password_confirmation: 'password123' } }
     
     post "/users/sign_in", params: { user: { email: 'test@example.com', password: 'password123' } }
+    token = response.headers['Authorization']
+
+    # post "/categories", params:{
+    #   name: "Chore"
+    # }, headers: { 'Authorization': token }
 
     assert_response :success
     assert_equal 1, User.count
+    # assert_equal 1, Category.count 
 
-    if response.status == :success
-      puts "PASSED"
-      elsif response.status == 401
-        puts "Failed test"
-    end
 
   end
 
-  # test "create category" do
-
-
-
-  # end
 end
